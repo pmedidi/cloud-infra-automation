8e430d09950a082b4e75dcf490c95d0f2d1207da
8d0ab68c.groups.gatech.edu@amer.teams.ms <pawan.medidi@gmail.com>
2025-03-24 19:07:39 -0400

    Sync: push all local changes to GitHub (infra, workflow, triggers)

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -16,6 +16,37 @@ resource "aws_subnet" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main[0].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.main[1].id
+  route_table_id = aws_route_table.public.id
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
